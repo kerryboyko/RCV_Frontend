fdd17630e8eb155c002bce77cd59078e8db75471
Brian Boyko <brian.boyko@gmail.com>
2019-04-16 16:17:30 -0400

    Added Douglas Adams Pullout

--- src/components/DouglasAdamsPullout.tsx
+++ src/components/DouglasAdamsPullout.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Figure from 'react-bootstrap/Figure';
 import copywriting from '../copywriting/frontPage';
 import VeryImportantLizard from '../img/VeryImportantLizard.svg';
+import './DouglasAdamsPullout.sass';
+
 const DouglasAdamsPullout = () => (
-  <div className="pullout">
-    <img className="pullout__image" src={VeryImportantLizard} />
-    <div>
-      {copywriting.EN.pullout.copy.map((line: string) => (
-        <div className="pullout__line">{line}</div>
-      ))}
-      <div className="pullout__credit">
-        <span className="pullout__credit__author">
-          {copywriting.EN.pullout.author}
-        </span>
-        <span className="pullout__credit__book">
-          {copywriting.EN.pullout.book}
-        </span>
-      </div>
-    </div>
-  </div>
+  <Card className="pullout">
+    <Card.Body>
+      <Card.Img
+        className="pullout__figure"
+        src={VeryImportantLizard}
+        alt={
+          'Very Important Lizard Gives A Speech -- by anarres: http:/foofurple.org'
+        }
+      />
+      <Card.Text>
+        {copywriting.EN.pullout.copy.map((line: string) => (
+          <div className="pullout__line">{line}</div>
+        ))}
+        <div className="pullout__credit">
+          --{' '}
+          <span className="pullout__credit__author">
+            {copywriting.EN.pullout.author}
+          </span>
+          :{' '}
+          <span className="pullout__credit__book">
+            {copywriting.EN.pullout.book}
+          </span>
+        </div>
+      </Card.Text>
+    </Card.Body>
+  </Card>
 );
 
 export default DouglasAdamsPullout;
